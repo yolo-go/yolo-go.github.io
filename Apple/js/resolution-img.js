fee14d076c0732b6529a60803f76f86978f182ca
yolo-go <77585717+yolo-go@users.noreply.github.com>
2021-01-18 20:34:07 +0200

    Add files via upload

---
+++ Apple/js/resolution-img.js
@@ -0,0 +1,41 @@
+﻿$.fn.dataImg = function(options) {
+
+  var settings = $.extend({
+    sml: 350,
+    med: 700,
+    lrg: 1450,
+    resize: false
+  }, options );
+
+  var elements = $(this);
+
+  function getSrc(element) {
+    var screen = $(window).width();
+    if (screen > settings.med) { 
+        return element.data('lrg');
+    }
+    else if (screen <= settings.med && screen > settings.sml) {
+        return element.data('med');
+    }
+    return element.data('sml');
+  }
+
+  function breakpoints() {
+    elements.each(function () {
+        var e = $(this);
+        var src = getSrc(e);
+        if(src != undefined){
+            if (e.is('img')) {
+                e.attr('src', src);
+            } 
+        }        
+    });
+  }breakpoints();
+
+  if(settings.resize == true){
+    $(window).resize(function(){
+      breakpoints();
+    });
+  }
+
+};
